1104b76796ef8cb6bf51f12cad95045e724be7f8
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-02-14 11:40:29 -0500

    finished user controllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,55 +1,74 @@
-const { Course, Student } = require('../models');
+const User = require('../models/User');
+const Thoughts = require('../models/Thought');
 
 module.exports = {
-    // Get all courses
-    getCourses(req, res) {
-        Course.find()
-            .then((courses) => res.json(courses))
+    // Get all users
+    getUsers(req, res) {
+        Users.find()
+            .select('-__v')
+            .then((user) => res.json(user))
             .catch((err) => res.status(500).json(err));
     },
-    // Get a course
-    getSingleCourse(req, res) {
-        Course.findOne({ _id: req.params.courseId })
+    // Get a user
+    getSingleuser(req, res) {
+        Users.findOne({ _id: req.params.userId })
             .select('-__v')
-            .then((course) =>
-                !course
-                    ? res.status(404).json({ message: 'No course with that ID' })
-                    : res.json(course)
+            .populate('thoughts')
+            .populate('friends')
+            .then((users) =>
+                !users
+                    ? res.status(404).json({ message: 'No users with that ID' })
+                    : res.json(users)
             )
             .catch((err) => res.status(500).json(err));
     },
-    // Create a course
-    createCourse(req, res) {
-        Course.create(req.body)
-            .then((course) => res.json(course))
+    // Create a user
+    createUser(req, res) {
+        Users.create(req.body)
+            .then((userData) => res.json(userData))
             .catch((err) => {
                 console.log(err);
                 return res.status(500).json(err);
             });
     },
-    // Delete a course
-    deleteCourse(req, res) {
-        Course.findOneAndDelete({ _id: req.params.courseId })
-            .then((course) =>
-                !course
-                    ? res.status(404).json({ message: 'No course with that ID' })
-                    : Student.deleteMany({ _id: { $in: course.students } })
+    // Delete a user
+    deleteUser(req, res) {
+        Users.findOneAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No users with that ID' })
+                    : Users.deleteMany({ _id: { $in: users.students } })
             )
-            .then(() => res.json({ message: 'Course and students deleted!' }))
+            .then(() => res.json({ message: 'User has been deleted!' }))
             .catch((err) => res.status(500).json(err));
     },
-    // Update a course
-    updateCourse(req, res) {
-        Course.findOneAndUpdate(
-            { _id: req.params.courseId },
+    // Update a user
+    updateUser(req, res) {
+        Users.findOneAndUpdate(
+            { _id: req.params.userId },
             { $set: req.body },
             { runValidators: true, new: true }
         )
-            .then((course) =>
-                !course
-                    ? res.status(404).json({ message: 'No course with this id!' })
-                    : res.json(course)
+            .select('-__v')
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No users with this id!' })
+                    : res.json(user)
             )
             .catch((err) => res.status(500).json(err));
+    },
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No users with this id!' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+
     },
 };
